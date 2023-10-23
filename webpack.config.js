4529ffbbb5294bb70030a4fe391996641aae7373
codelande <corentin.delande.1@gmail.com>
2023-10-23 12:05:58 +0200

    add some style

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addStyleEntry('style', './assets/styles/app.css')
+
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
